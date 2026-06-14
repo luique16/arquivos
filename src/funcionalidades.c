9335b0073b7ccf78cef18193f26a571806c08b56
Luís Henrique <luish.fa1515@gmail.com>
2026-06-14 06:02:58 -0300

    comentarios

--- src/funcionalidades.c
+++ src/funcionalidades.c
@@ -504,19 +504,23 @@ void funcionalidade8() {
 }
 
 void funcionalidade9() {
+    /* Nome dos arquivos de dados e índice */
     char nomeArquivoDados[256];
     char nomeArquivoIndice[256];
     scanf("%s %s", nomeArquivoDados, nomeArquivoIndice);
 
+    /* Número de inserções */
     int n;
     scanf("%d", &n);
 
+    /* Abre o arquivo de dados */
     FILE *fpDados = fopen(nomeArquivoDados, "r+b");
     if (fpDados == NULL) {
         printf("Falha no processamento do arquivo.\n");
         return;
     }
 
+    /* Lê cabeçalho do arquivo de dados */
     CabecalhoArquivo cabDados;
     lerCabecalho(fpDados, &cabDados);
     if (cabDados.status == STATUS_INCONSISTENTE) {
@@ -525,10 +529,13 @@ void funcionalidade9() {
         return;
     }
 
+    /* Marca arquivo de dados como inconsistente */
     marcarInconsistente(fpDados);
 
+    /* Abre o arquivo de índice */
     FILE *fpIndice = fopen(nomeArquivoIndice, "r+b");
 
+    /* Lê cabeçalho do arquivo de índice */
     CabecalhoArvB cabIndice;
     cabIndice = lerCabecalhoArvB(fpIndice);
 
@@ -539,6 +546,7 @@ void funcionalidade9() {
         return;
     }
 
+    /* Marca arquivo de índice como inconsistente */
     cabIndice.status = STATUS_INCONSISTENTE;
     escreverCabecalhoArvB(fpIndice, cabIndice);
 
@@ -554,38 +562,48 @@ void funcionalidade9() {
             }
         }
 
+        /* Seleciona o RRN de destino (topo da pilha ou proxRRN) */
         int rrnDestino;
         if (cabDados.topo != INTEIRO_NULO) {
+            /* Reaproveita topo da pilha de removidos */
             rrnDestino = cabDados.topo;
             RegistroDados regRemovido;
             lerRegistro(fpDados, &regRemovido, rrnDestino);
-            cabDados.topo = regRemovido.proximo;
+            cabDados.topo = regRemovido.proximo; /* desempilha */
         } else {
+            /* Pilha vazia: insere ao final */
             rrnDestino = cabDados.proxRRN;
             cabDados.proxRRN++;
         }
 
+        /* Incrementa contador se codEstacao e codProxEstacao não forem nulos */
         if (novoReg.codEstacao != INTEIRO_NULO && novoReg.codProxEstacao != INTEIRO_NULO) {
             cabDados.nroParesEstacao++;
         }
 
+        /* Marca como ativo e sem próximo na pilha */
         novoReg.removido = REGISTRO_ATIVO;
         novoReg.proximo  = INTEIRO_NULO;
 
+        /* Escreve registro no arquivo de dados */
         int offset = (int)rrnParaOffset(rrnDestino);
         escreverRegistro(fpDados, &novoReg, rrnDestino);
 
+        /* Insere chave no índice */
         inserirNaArvore(fpIndice, &cabIndice, novoReg.codEstacao, offset);
     }
 
+    /* Finaliza arquivo de dados */
     atualizarCabecalho(fpDados, &cabDados);
     escreverCabecalho(fpDados, &cabDados);
     fecharArquivoBin(fpDados);
 
+    /* Finaliza arquivo de índice */
     cabIndice.status = STATUS_CONSISTENTE;
     escreverCabecalhoArvB(fpIndice, cabIndice);
     fclose(fpIndice);
 
+    /* Exibe binários */
     BinarioNaTela(nomeArquivoDados);
     BinarioNaTela(nomeArquivoIndice);
 }
